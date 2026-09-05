bd54ff29876899d42fe191f6a012700951c5841d
Andrew Manteau <157993039+AndrewManteau@users.noreply.github.com>
2026-09-05 16:38:59 -0400

    Update comments in Lab 2 code

--- Lab 2/Blink_Test/src/main.cpp
+++ Lab 2/Blink_Test/src/main.cpp
@@ -5,15 +5,15 @@
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
-  // TODO: Define your baud rates
+  // AJM451 - define baud rate as suggested in the lab instructions
   Serial.begin(115200); // Start the Serial connection
 }
 
 void loop() {
-  // TODO: Define your text output
+  // AJM451 - print a custom message to the serial monitor
   Serial.println("Hello Feather -Andrew"); // Print the words to the Serial Monitor.
   
-  // TODO: Set up your delay in milliseconds
+  // AJM451 - set the blink pattern to do a flashy blink, see the video in Lab 2 folder for the output
   digitalWrite(LED_PIN, HIGH); // LED On
   delay(250); // Speed in milliseconds
   digitalWrite(LED_PIN, LOW); // LED Off
